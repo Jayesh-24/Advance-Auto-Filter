71a6f2f0ef3747bc9425157735fac051ea693b67
Jayesh-24 <87383490+Jayesh-24@users.noreply.github.com>
2024-05-13 12:36:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,13 +98,14 @@ async def start(client, message):
                                  url=f'https://t.me/Jayesh_19')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        reply_markup = InlineKeyboardMarkup(buttons)
-await message.reply(
-    text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-    reply_markup=reply_markup,
-    parse_mode=enums.ParseMode.HTML
-)
-return
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+        return
+
 
     data = message.command[1]
     try:
