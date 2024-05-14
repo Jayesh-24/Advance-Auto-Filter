3abc6f23e450bccd3d62e1677a147bb09f9af706
Jayesh-24 <87383490+Jayesh-24@users.noreply.github.com>
2024-05-14 11:03:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1436,7 +1436,7 @@ async def auto_filter(client, msg, spoll=False):
     TEMPLATE = settings['template']
     if imdb:
         cap = TEMPLATE.format(
-            query=search,
+            qurey=search,
             title=imdb['title'],
             votes=imdb['votes'],
             aka=imdb["aka"],
